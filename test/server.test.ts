9183480a26b3df5438a33fefefa084e41c99bce8
bitfishxyz <bitfishxyz@gmail.com>
2019-06-06 11:43:29 +0800

    添加测试代码覆盖率的选项

--- test/server.test.ts
+++ test/server.test.ts
@@ -6,7 +6,7 @@ import server from "../src/server";
 describe("测试http服务器", () => {
     it("Get /", async () => {
         const result = await request(server).get("/");
-        console.log(result);
+        // console.log(result);
         expect(result.text).toEqual("hello world");
         expect(result.status).toEqual(200);
     });
